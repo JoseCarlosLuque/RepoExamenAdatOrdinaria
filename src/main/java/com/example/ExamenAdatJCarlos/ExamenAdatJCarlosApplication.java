b7fb971b1c4d4c1021d44fe811dbe7878edd4f08
JoseCarlos <luquecastrojosecarlos@gmail.com>
2025-03-11 09:14:48 +0100

    Primer commit del proyecto

---
+++ src/main/java/com/example/ExamenAdatJCarlos/ExamenAdatJCarlosApplication.java
@@ -0,0 +1,13 @@
+package com.example.ExamenAdatJCarlos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenAdatJCarlosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenAdatJCarlosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ExamenAdatJCarlos/ExamenAdatJCarlosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ExamenAdatJCarlos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenAdatJCarlosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
